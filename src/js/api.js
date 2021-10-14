c0fb8ccefa7b5c9fd599c53df20d2083ac906aae
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-14 11:32:51 +0200

    Implement postLikes function to send likes to the API

--- src/js/api.js
+++ src/js/api.js
@@ -1,9 +1,27 @@
+// eslint-disable-next-line import/no-cycle
 import { renderNews } from './dom.js';
 
 const url = 'https://api.spaceflightnewsapi.net/v3/articles';
+const api = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EXlTe26DlB9I8ip8IWwa/likes';
 
 export const getResponse = async () => {
   const get = await fetch(url);
   const response = await get.json();
   renderNews(response);
 };
+
+export const postLikes = async (id, numbers) => {
+  const post = await fetch(api, {
+    method: 'POST',
+    body: JSON.stringify({ item_id: id }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const response = await post.text();
+  console.log(response);
+  return response;
+};
+
+// postLikes(4);
+
