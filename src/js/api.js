5046add57bbaeb182b3b7a74178cf27aaf9d6cf5
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-10-14 14:44:39 +0200

    Modify the api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,40 +1,40 @@
-// eslint-disable-next-line import/no-cycle
 import { renderNews } from './dom.js';
 
-const url = 'https://api.spaceflightnewsapi.net/v3/articles';
-const api = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EXlTe26DlB9I8ip8IWwa/likes';
+const BASE_URL = 'https://api.spaceflightnewsapi.net/v3/articles';
+const involvementAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EXlTe26DlB9I8ip8IWwa/likes';
 
 export const getResponse = async () => {
-  const get = await fetch(url);
+  const get = await fetch(BASE_URL);
   const response = await get.json();
   renderNews(response);
 };
 
-export const postLikes = async (id, numbers) => {
-  const post = await fetch(api, {
-    method: 'POST',
-    body: JSON.stringify({ item_id: id }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  const response = await post.text();
-  // console.log(response);
-  getLikes(id, numbers);
-  return response;
+export const getLikes = async () => {
+  const response = await fetch(involvementAPI);
+  if (!response.ok) {
+    throw new Error(`API error! status: ${response.status}`);
+  } else {
+    const data = await response.json();
+    localStorage.setItem('likes', JSON.stringify(data));
+    return data;
+  }
 };
 
-// postLikes(4);
-
-export const getLikes = async (idLike, numberLikes) => {
-  const put = await fetch(api);
-  const response = await put.json();
-  // console.log(idLike, numberLikes);
-
-  response.forEach((element) => {
-    if (element.item_id === idLike.toString()) {
-      // console.log(element);
-      numberLikes.innerHTML = element.likes;
-    }
-  });
+export const postLikes = async (id) => {
+  try {
+    const response = await fetch(involvementAPI, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: id,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.text())
+      .then((json) => json);
+    return response;
+  } catch (e) {
+    throw new Error(`API error! status: ${e.toString()}`);
+  }
 };
